8e6510f62d3737cdc3a8df944ec8be4c6ea80044
João Victor Bezerra Batista Rocha <107816755+jvrocha91@users.noreply.github.com>
2023-10-02 01:27:21 -0300

    helloWord

---
+++ first/src/first/Main.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x=8;
+		System.out.print("Hello Word, ");
+		System.out.println("Bom dia!");
+		System.out.println(x);
+		System.out.printf("JV %d JV",x);
+
+	}
+
+}
